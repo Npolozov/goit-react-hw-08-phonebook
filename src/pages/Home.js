dfc0030b9183ffeff961ebec362ce5c3acb3b6b9
Nikita Polozov <npolozov0@gmail.com>
2023-02-10 18:44:44 +0200

    fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,27 @@
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const styles = {
-  container: {
-    marginTop: 200,
-  },
   title: {
+    marginTop: 200,
     fontWeight: 500,
     fontSize: 48,
+    textAlign: 'center',
+  },
+  titleMob: {
+    marginTop: 100,
+    fontWeight: 500,
+    fontSize: 28,
     textAlign: 'center',
   },
   bottomtext: {
     fontWeight: 400,
     fontSize: 28,
+    textAlign: 'center',
+  },
+  bottomtextMob: {
+    fontWeight: 300,
+    fontSize: 24,
     textAlign: 'center',
   },
   link: {
@@ -21,12 +31,22 @@ const styles = {
 };
 
 export const Home = () => {
+  const [matches, setMatches] = useState(
+    window.matchMedia('(max-width: 768px)').matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia('(max-width: 768px)')
+      .addEventListener('change', e => setMatches(e.matches));
+  }, []);
+
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>
+      <h1 style={!matches ? styles.title : styles.titleMob}>
         This is a react app for saving your private contacts.
       </h1>
-      <p style={styles.bottomtext}>
+      <p style={!matches ? styles.bottomtext : styles.bottomtextMob}>
         You can{' '}
         <NavLink style={styles.link} to="/register">
           register
