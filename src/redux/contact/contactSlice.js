1bb709fe741704c40d8fe609f70a8dde67ff4611
Nikita Polozov <npolozov0@gmail.com>
2023-02-16 21:12:36 +0200

    fix

--- src/redux/contact/contactSlice.js
+++ src/redux/contact/contactSlice.js
@@ -75,3 +75,73 @@ export const contactsSlice = createSlice({
 });
 
 export const contactsReducer = contactsSlice.reducer;
+
+// import { createSlice } from '@reduxjs/toolkit';
+// import {
+//   fetchContacts,
+//   addContact,
+//   deleteContact,
+//   updateContact,
+// } from './contactOperations';
+// import { logOut } from 'redux/auth/Operations';
+
+// const handlePending = state => {
+//   state.isLoading = true;
+// };
+
+// const handleRejected = (state, action) => {
+//   state.isLoading = false;
+//   state.error = action.payload;
+// };
+
+// export const contactsSlice = createSlice({
+//   name: 'contacts',
+//   initialState: {
+//     items: [],
+//     isLoading: false,
+//     error: null,
+//   },
+
+//   extraReducers: {
+//     [fetchContacts.pending]: handlePending,
+//     [addContact.pending]: handlePending,
+//     [deleteContact.pending]: handlePending,
+//     [fetchContacts.rejected]: handleRejected,
+//     [addContact.rejected]: handleRejected,
+//     [deleteContact.rejected]: handleRejected,
+//     [fetchContacts.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.items = action.payload;
+//     },
+//     [addContact.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       state.items.push(action.payload);
+//     },
+//     [deleteContact.fulfilled](state, action) {
+//       state.isLoading = false;
+//       state.error = null;
+//       const index = state.items.findIndex(
+//         task => task.id === action.payload.id
+//       );
+//       state.items.splice(index, 1);
+//     },
+//     [updateContact.fulfilled](state, action) {
+//       state.items.map(item => {
+//         if (item.id === action.payload.id) {
+//           item.name = action.payload.name;
+//           item.number = action.payload.number;
+//         }
+//         return item;
+//       });
+//     },
+//     [logOut.fulfilled](state) {
+//       state.items = [];
+//       state.error = null;
+//       state.isLoading = false;
+//     },
+//   },
+// });
+
+// export const contactsReducer = contactsSlice.reducer;
